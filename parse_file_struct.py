1bd1ea0cc1135e207ec17305a133374900da89c7
Luke Everhart <luke.f.everhart@gmail.com>
2026-03-19 21:55:56 -0400

    Add file parsing script to create our input imagery file.

---
+++ parse_file_struct.py
@@ -0,0 +1,18 @@
+# Usage: parse_file_struct.py [/path/to/root/] [/path/to/output]
+
+from sys import argv
+import os
+
+path = argv[1]
+
+imagery_files = []
+
+for (root, dirs, file) in os.walk(path):
+    for f in file:
+        if ".ntf" in f:
+            imagery_files.append(os.path.join(root, f))
+
+
+with open(argv[2], 'w') as f:
+    for line in imagery_files:
+        f.write(line)
